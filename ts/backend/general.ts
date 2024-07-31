5239b228960c181cd485398135f9c579ffd33b70
jessetvogel <jesse.t.vogel@gmail.com>
2024-07-31 14:45:48 +0200

    process.stdout.clearLine is not a function

--- ts/backend/general.ts
+++ ts/backend/general.ts
@@ -15,7 +15,7 @@ export class Log {
 
         fn();
 
-        process.stdout.clearLine(0);
+        // process.stdout.clearLine(0);
         process.stdout.cursorTo(0);
         process.stdout.write(`✅ ${msg}    \n`);
     }
